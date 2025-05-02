8d4c8b7b1e4d3757fc4cff0df9267ff8173f4ac1
Asif Baba Mohammad <asifbaba.mohammad@infor.com>
2025-05-02 11:15:20 +0530

    added scripts in package.json

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: 'u2c3oy',
+  projectId: "u2c3oy",
   video: true,
   e2e: {
+    defaultCommandTimeout: 8000,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
+      return config; // 👈 Make sure to return the config
     },
+    reporter: "cypress-mochawesome-reporter",
+    reporterOptions: {
+      reportDir: "cypress/reports/mochawesome",
+      overwrite: false,
+      html: false,
+      json: true,
+    }
   },
 });
